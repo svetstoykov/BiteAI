b7d97340ef325d60e1983e7d7e4ac47347cb90ca
Svetlozar Stoykov <deucyy@gmail.com>
2025-03-05 08:59:49 +0200

    Remove Description and Name properties from Meals and MealPlans for schema simplification

---
+++ server/BiteAI.Infrastructure/Migrations/20250305065845_MealModelsUpdate.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiteAI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class MealModelsUpdate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Meals");
+
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "MealPlans");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Meals",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "MealPlans",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
